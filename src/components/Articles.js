e97368355556c9831e7a4c5fd6bfa51ff878bea2
Christoph Nientied <christophnientied@googlemail.com>
2023-04-18 18:10:24 +0200

    set up Articles.js and fetch of API

---
+++ src/components/Articles.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export default function Articles () {
+
+    const [articles, setArticles] = useState([]);
+  
+    //1. Mit Fetch
+    useEffect(() => {
+      //hier geht der Fetch los
+      fetch("https://hn.algolia.com/api/v1/search?query=javascript")
+        .then((res) => {
+          if (!res.ok) throw new Error(`Failed because ${res.status}`);
+          console.log(res);
+          return res.json();
+        })
+        .then((data) => {
+          //WICHTIG: CONSOLE CHECKEN!
+          console.log("inside 2nd then:", data);
+          // data.hits: Es wird ein Weg gesucht, auf diese data.hits zuzugreifen, damit es im Objektbaum
+          setArticles(data);
+        })
+        .catch((err) => console.log(err));
+    }, []);
+
+
+    return(
+        <div>
+            <h1>JS Articles</h1>
+            {articles.length 
+            ? articles.hits.map((article)=> {
+                return <li>article.title</li>;
+            })
+            : "...loading" }
+        </div>
+    )
+}
+
+
