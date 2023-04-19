3d870c047b9d0d1949748d794c6a47e98c2c374f
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-04-19 14:15:42 +0200

    Adding the Articles Component and fetching data from the hackernews api

---
+++ src/components/Articles.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+export default function Articles() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    //hier geht der Fetch los
+    fetch("http://hn.algolia.com/api/v1/search_by_date?query=javascript")
+      .then((res) => {
+        if (!res.ok) throw new Error(`Failed because ${res.status}`);
+        // console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        //WICHTIG: CONSOLE CHECKEN!
+        console.log("inside 2nd then:", data);
+        setArticles(data.hits);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hackernews</h1>
+      {articles.length
+        ? articles.map((article) => {
+            return (
+              <li>
+                <h4>{article.story_title}</h4>
+                <div>
+                  <p>{format(article.created_at_i, "dd/mm/yyyy")}</p>
+                  <a href={article.url || article.story_url} target="_blank">
+                    Read Full Article
+                  </a>
+                </div>
+              </li>
+            );
+          })
+        : "...loading"}
+    </div>
+  );
+}
