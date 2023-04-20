5ccf246798c9fa476b380b39e8386b9af520aad8
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-04-20 17:25:27 +0200

    formatted date

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Search from "./components/Search";
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("javascript");
 
   useEffect(() => {
     //hier geht der Fetch los
-    fetch(`http://hn.algolia.com/api/v1/search/?query=${query}&hitsPerPage=50`)
+    fetch(
+      `http://hn.algolia.com/api/v1/search/?query=${query}&tags=story&hitsPerPage=50`
+    )
       .then((res) => {
         if (!res.ok) throw new Error(`Failed because ${res.status}`);
         // console.log(res);
@@ -27,30 +29,32 @@ function App() {
       <div>
         <h1>Hackernews</h1>
         <Search setQuery={setQuery} />
-        {articles.length
-          ? articles.map((article) => {
-              return (
-                <ol>
-                  <li>
-                    <h4>{article.story_title || article.title}</h4>
-                    <div>
-                      <p>
-                        Blog article date:{" "}
-                        {format(article.created_at_i, "dd.mm.yyyy")}
-                      </p>
-                      <a
-                        href={article.url || article.story_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Read Full Article
-                      </a>
-                    </div>
-                  </li>
-                </ol>
-              );
-            })
-          : "...loading"}
+        <ol>
+          {articles.length
+            ? articles.map((article) => {
+                return (
+                  <>
+                    <li>
+                      <h4>{article.story_title || article.title}</h4>
+                      <div>
+                        <p>
+                          Blog article date:{" "}
+                          {format(new Date(article.created_at), "dd.MM.yy")}
+                        </p>
+                        <a
+                          href={article.url || article.story_url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Read Full Article
+                        </a>
+                      </div>
+                    </li>
+                  </>
+                );
+              })
+            : "...loading"}
+        </ol>
       </div>
     </>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
 export default function Search(props) {
-  const [inputValue, setInputValue] = useState("javascript");
+  const [inputValue, setInputValue] = useState("");
 
   // console.log(query + " Ergebnis");
 
   const addQuery = (event) => {
     event.preventDefault();
     if (inputValue.trim() === "") {
-      return;
+      alert("Your search field is empty!");
+      // return;
     }
 
     props.setQuery(inputValue.trim());
